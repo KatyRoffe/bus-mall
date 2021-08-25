d44b4bfb47be404aadfb6f0ff8394b908deca4ef
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-08-24 22:58:35 -0700

    complete functions to make images render

--- app.js
+++ app.js
@@ -1,130 +1,118 @@
 'use strict';
 
-// console.log('AHHHHHH')
+// console.log('AHHHHHHHH!');
 
-// product constructor function
 
-// display products with a prototype method
-// display 3 products
-// randomize products
-// product array
-// product vote counter
-// convert votes to percentages
-// determine which picture cliked on
-// event listen & clickHander
-// updates global counter
-// convert votes to percentage
+// global variables
 
-// -------------------- Global Variables --------------------//
-const leftProductImgElement = document.getElementById('leftProductImg');
-const leftProductH3Element = document.getElementById('leftProductH3');
-const middleProductImgElement = document.getElementById('middleProductImg');
-const middleProductH3Element = document.getElementById('middleProductH3');
-const rightProductImgElement = document.getElementById('rightProductImg');
-const rightProductH3Element = document.getElementById('rightProductH3');
+const leftImageElement = document.getElementById('leftImg');
+const centerImageElement = document.getElementById('centerImg');
+const rightImageElement = document.getElementById('rightImg');
+const productSectionElement = document.getElementById('products');
 
 let leftProduct = null;
-let middleProduct = null;
 let rightProduct = null;
+let centerProduct = null;
 
-let rounds = 25
+let rounds = 25;
 
-// -------------------- Constructor Function --------------------//
+// constructor function
 
 function Product(name, image) {
   this.name = name;
   this.image = image;
   this.timesShown = 0;
-  this.votes = 0;
+  this.timesClicked = 0;
 }
 
-// -------------------- Prototype --------------------//
-
-Product.prototype.renderProduct = function (img, h3) {
-  img.src = this.image;
-  h3.textContent = this.name;
+// protoype
+Product.allProducts = [];
+Product.prototype.renderOneProduct = function(imageElement) {
+  imageElement.src = this.image;
   this.timesShown++;
 }
 
-Product.allProducts = [];
-
-// -------------------- Global Functions --------------------//
-function randomProducts () {
-  let leftProduct = Math.floor(Math.random() * Product.allProducts.length);
-  let middleProduct = Math.floor(Math.random() * Product.allProducts.length)
-  let rightProduct = Math.floor(Math.random() * Product.allProducts.length);
-
-    while (leftProduct === undefined || leftProduct === rightProduct) {
-      leftProduct = Math.floor(Math.random() * Product.allProducts.length);
-    }
-    while (middleProduct === undefined || middleProduct === leftProduct || middleProduct === rightProduct) {
-      middleProduct = Math.floor(Math.random() * Product.allProducts.length);
-    }
-  }
-
-  leftProduct = Product.allProducts[leftProduct]
-  middleProduct = Product.allProducts[middleProduct];
-  rightProduct = Product.allProducts[rightProduct]
-
-renderThreeProducts(leftProduct, middleProduct, rightProduct);
-
-function renderThreeProducts(leftProduct, middleProduct, rightProduct) {
-  leftProduct.renderProduct(leftProductImgElement, leftProductH3Element);
-  middleProduct.renderProduct(middleProductImgElement, middleProductH3Element);
-  rightProduct.renderProduct(rightProductImgElement, rightProductImgElement);
+// functions
+function makeNewProduct(name, image) {
+  Product.allProducts.push(new Product(name, image));
 }
 
-function clickHandler(event) {
+function renderThreeProducts() {
+  const leftChoice = Math.floor(Math.random() * Product.allProducts.length);
+  leftProduct = Product.allProducts[leftChoice];
+
+  while (!rightProduct || rightProduct === leftProduct) {
+    const rightChoice = Math.floor(Math.random() * Product.allProducts.length);
+    rightProduct = Product.allProducts[rightChoice];
+  }
+
+  while (!centerProduct || centerProduct === leftProduct || centerProduct === rightProduct) {
+    const centerChoice = Math.floor(Math.random() * Product.allProducts.length);
+    centerProduct = Product.allProducts[centerChoice];
+  }
+
+  leftProduct.renderOneProduct(leftImageElement);
+  rightProduct.renderOneProduct(rightImageElement);
+  centerProduct.renderOneProduct(centerImageElement);
+
+}
+
+function renderResults() {
+  const resultsElement = document.getElementById('results');
+  resultsElement.innerHTML = '';
+  for (let product of Product.allProducts) {
+    const liElement = document.createElement('li');
+    liElement.textContent = `${product.name} was shown ${product.timesShown} times and was clicked ${product.timesClicked} times.`;
+    resultsElement.appendChild(liElement);
+  }
+}
+
+
+function handleClick(event) {
   console.log(event.target);
-  if (event.target === leftProductImgElement || event.target === middleProductImgElement || rightProductImgElement) {
+  const validTargets = [leftImageElement, rightImageElement, centerImageElement];
+  if (validTargets.includes(event.target)) {
     rounds--;
-    if (event.target === leftProductImgElement) {
-        leftProduct.votes++;
-      } else if (event.target === rightProductImgElement) {
-        rightProduct.votes++;
-      } else {
-        middleProduct.votes++;
+    if (event.target === validTargets[0]) {
+      validTargets[0].timesClicked++;
+    } else if (event.target === validTargets[1]) {
+      validTargets[1].timesClicked++;
+    } else {
+      validTargets[2].timesClicked++;
     }
     if (rounds === 0) {
-      allProductsSectionElem.removeEventListener(click.clickHandler)
+      productSectionElement.removeEventListener('click', handleClick);
+      alert('Thank you for voting!');
+      renderResults();
+    } else {
+      renderThreeProducts();
     }
-    renderResults();
   }
-  randomProducts();
 }
 
-function renderRestults() {
-  const ulElement = document.getElementById('l1')
-  leftProductImgElement.textContent = `${product.name}: ${product.votes}`
-  ulElement.appendChild(liElement);
-}
+//listeners
+productSectionElement.addEventListener('click', handleClick);
 
 
+//call functions
+makeNewProduct('Bag2D2', './img/bag.jpg');
+makeNewProduct('Banana Slicer', './img/banana.jpg');
+makeNewProduct('Bathroom Media', './img/bathroom.jpg');
+makeNewProduct('Bootles', './img/boots.jpg');
+makeNewProduct('Breakfast3000', './img/breakfast.jpg');
+makeNewProduct('BubbleMeat Gum', './img/bubblegum.jpg');
+makeNewProduct('Chairwow', './img/chair.jpg');
+makeNewProduct('Cthulhu Figure', './img/cthulhu.jpg');
+makeNewProduct('Doggie Duck Lips', './img/dog-duck.jpg');
+makeNewProduct('Dragon Meat', './img/dragon.jpg');
+makeNewProduct('Pentensils', './img/pen.jpg');
+makeNewProduct('Pwiffer', './img/pet-sweep.jpg');
+makeNewProduct('Pizzers', './img/scissors.jpg');
+makeNewProduct('SharkBagO', './img/shark.jpg');
+makeNewProduct('Swonsie', './img/sweep.png');
+makeNewProduct('Tauntaun Bag', './img/tauntaun.jpg');
+makeNewProduct('Unicorn Meat', './img/unicorn.jpg');
+makeNewProduct('Watering Can', './img/water-can.jpg');
+makeNewProduct('Wine Glass', './img/wine-glass.jpg');
 
-// -------------------- Listeners --------------------//
-
-allProductsSectionElem.addEventListener('click', clickHander)
-
-// -------------------- Call Function --------------------//
-
-new Product('Bag2D2', './img/bag.jpg');
-new Product('BananaSlicer', './img/banana.jpg');
-new Product('BathroomReading', './img/bathroom.jpg')
-new Product('Bootdles', './img/boots.jpg')
-new Product('BreakfastInOne', './img/breakfast.jpg')
-new Product('BubblegumMeatballs', './img/bubblegum.jpg')
-new Product('ChairSupreme', './img/chair.jpg')
-new Product('CthulhuFigure', './img/cthulhu.jpg')
-new Product('DoggieDuckLips', './img/dog-duck.jpg')
-new Product('DragonMeat', './img/dragon.jpg')
-new Product('Pentensils', './img/pen.jpg')
-new Product('PetSweep', './img/pet-sweep.jpg')
-new Product('Pissors', './img/scissors.jpg')
-new Product('SharkBag', './img/shark.jpg')
-new Product('Swiffsie', './img/sweep.png')
-new Product('TauntaunBag', './img/tauntaun.jpg')
-new Product('UnicornMeat', './img/unicorn.jpg')
-new Product('WateringCan', './img/water-can.jpg')
-new Product('Wine-OGlass', './img/wine-glass.jpg')
-
-randomProducts();
+renderThreeProducts();
