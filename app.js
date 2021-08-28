3a2188dda1fe99dc756b0d4ff1a7c9993fbdf4cf
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-08-28 03:13:41 -0700

    Coding adjustments. These don't work, but pushing anyways to create a new branch and start clean.

--- app.js
+++ app.js
@@ -192,3 +192,173 @@ var myChart = new Chart(ctx, {
     }
 });
 }
+
+// local storage
+
+function getProductsFromStorage() {
+  const stringifiedProducts = localStorage.getItem('productsInStorage')
+  if(stringifiedProducts) {
+    const parsedProducts = JSON.parse(stringifiedProducts)
+    console.log(parsedProducts)
+    for(let Product of parsedProducts) {
+      const myProduct = new Item(item.name, item.image);
+      Product.parsedProducts.push(myProduct);
+    }
+    renderResults();
+  } else {
+    alert('no productst');
+  }
+}
+
+function storeProducts() {
+  const stringifiedProducts = JSON.stringify(Product.allProducts);
+  console.log(stringifiedProducts)
+  localStorage.setProduct('itemsInStorage', stringifiedProducts)
+}
+
+getProductsFromStorage();
+
+
+
+
+
+
+// trying something new below, but it's not working too well either... 
+
+// 'use strict'
+
+// // console.log('All hail the mighty glow cloud.')
+
+// //global variables
+
+// const leftImageElement = document.getElementById('leftImg');
+// const centerImageElement = document.getElementById('centerImg');
+// const rightImageElement = document.getElementById('rightImg');
+
+// const leftH3Elem = document.getElementById('leftH3');
+// const centerH3Elem = document.getElementById('centerH3');
+// const rightH3Elem = document.getElementById('rightH3');
+
+
+// const productSectionElement = document.getElementById('products'); 
+
+// let leftProduct = null;
+// let rightProduct = null;
+// let centerProduct = null;
+
+// let rounds = 25;
+
+// //constructor function
+
+// function Product(name, image) {
+//   this.name = name;
+//   this.image = image;
+//   this.userViews = 0;
+//   this.userClicks = 0;
+// }
+
+
+
+// //prototype methods
+// Product.allproducts = [];
+
+// Product.prototype.renderOneProduct = function(imageElement, h3Element) {
+//   imageElement.src = this.image;
+//   h3Element.textContent = this.name;
+//   this.userViews++;
+// }
+
+// // functions
+// function makeNewProduct(name, image) {
+//   Product.allProducts.push(new Product(name, image));
+// }
+
+// function getRandomProducts(){
+//   const recycleProducts = [leftProduct, centerProduct, rightProduct];
+//   while(recycleProducts.includes(leftProduct)) {
+//     let leftOption = Math.floor(Math.random () * Product.allproducts.length);
+//     leftProduct = Product.allproducts[leftOption];
+//   }
+//   recycleProducts.push(leftProduct)
+//   while(recycleProducts.includes(centerProduct)) {
+//     let centerOption = Math.floor(Math.random () * Product.allproducts.length);
+//     centerProduct = Product.allproducts[centerOption];
+//   }
+//   recycleProducts.push(centerProducts)
+//   while(recycleProducts.includes(rightProtduct)) {
+//     let rightOption = Math.floor(Math.random () * Product.allproducts.length);
+//     rightProtduct = Product.allproducts[rightOption];
+//   }
+//   renderAllItems();
+// }
+
+// function renderThreeProducts() {
+//   leftProduct.renderOneProduct(leftImageElement, leftH3Elem);
+//   centerProduct.renderOneProduct(centerImageElement, centerH3Elem);
+//   rightProduct.renderOneProduct(rightImageElement, rightH3Elem);
+// }
+
+// function clickHandle(event) {
+//   let yesClick = event.target.id;
+//   // console.log(yesClick);
+//   if (yesClick === 'leftImg' || yesClick === 'middleImg' || yesClick === 'rightImg') {
+//     rounds--;
+//     if (yesClick === 'leftImg') {
+//       leftProduct.votes++;
+//     } else if (yesClick === 'middleImg') {
+//       centerProduct.votes++;
+//     } else if (yesClick === 'rightImg') {
+//       rightProduct.votes++;
+//     }
+//   } else {
+//     alert('Invalid vote.');
+//   }
+//   if (rounds === 0) {
+//     allProductSectionElement.removeEventListener('click', clickHandle);
+//     alert('Thank you for voting. Results now available for viewing.');
+//   } else {
+//     renderResults();
+//     getRandomProducts();
+//     renderThreeProducts();
+//   }
+// }
+
+// function renderResults() {
+//   const resultsElement = document.getElementById('results');
+//   resultsElement.innerHTML = '';
+//   for (let product of Product.allProducts) {
+//     const liElement = document.createElement('li');
+//     liElement.textContent = `${product.name} was shown ${product.timesShown} times and was clicked ${product.timesClicked} times.`;
+//     resultsElement.appendChild(liElement);
+//   }
+// }
+
+// //listeners
+
+// productSectionElement.addEventListener('click', clickHandle);
+
+// // call functions
+
+// new Product('Bag2D2', './img/bag.jpg');
+// new Product('BananaSlicer', './img/banana.jpg');
+// new Product('BathroomMedia', './img/bathroom.jpg');
+// new Product('Bootles', './img/boots.jpg');
+// new Product('Breakfast3000', './img/breakfast.jpg');
+// new Product('BubbleMeatGum', './img/bubblegum.jpg');
+// new Product('Chairwow', './img/chair.jpg');
+// new Product('CthulhuFigure', './img/cthulhu.jpg');
+// new Product('DoggieDuckLips', './img/dog-duck.jpg');
+// new Product('DragonMeat', './img/dragon.jpg');
+// new Product('Pentensils', './img/pen.jpg');
+// new Product('Pwiffer', './img/pet-sweep.jpg');
+// new Product('Pizzers', './img/scissors.jpg');
+// new Product('SharkBagO', './img/shark.jpg');
+// new Product('Swonsie', './img/sweep.png');
+// new Product('TauntaunBag', './img/tauntaun.jpg');
+// new Product('Unicorn Meat', './img/unicorn.jpg');
+// new Product('Watering Can', './img/water-can.jpg');
+// new Product('Wine Glass', './img/wine-glass.jpg');
+
+// renderThreeProducts();
+// // renderChart();
+
